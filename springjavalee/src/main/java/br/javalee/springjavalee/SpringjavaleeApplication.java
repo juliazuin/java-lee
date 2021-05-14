8b15d0d7bf8a6c4ec5328dea4243ba99bec4a1b8
Julia Zuin <zuinjulia@hotmail.com>
2021-05-14 15:51:59 -0300

    iniciando projeto spring

---
+++ springjavalee/src/main/java/br/javalee/springjavalee/SpringjavaleeApplication.java
@@ -0,0 +1,13 @@
+package br.javalee.springjavalee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringjavaleeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringjavaleeApplication.class, args);
+	}
+
+}
---
+++ springjavalee/src/test/java/br/javalee/springjavalee/SpringjavaleeApplicationTests.java
@@ -0,0 +1,13 @@
+package br.javalee.springjavalee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringjavaleeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
