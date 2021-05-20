05957173b2ff06ab65abbab063fff8a1014d249d
Julia Zuin <zuinjulia@hotmail.com>
2021-05-20 10:23:43 -0300

    merge em models

--- springjavalee/src/main/java/br/javalee/springjavalee/models/Usuario.java
+++ springjavalee/src/main/java/br/javalee/springjavalee/models/Usuario.java
@@ -1,4 +1,4 @@
-package br.javalee.springjavalee.model;
+package br.javalee.springjavalee.models;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
--- springjavalee/src/main/java/br/javalee/springjavalee/repo/UsuarioRepo.java
+++ springjavalee/src/main/java/br/javalee/springjavalee/repo/UsuarioRepo.java
@@ -2,7 +2,7 @@ package br.javalee.springjavalee.repo;
 
 import org.springframework.data.repository.CrudRepository;
 
-import br.javalee.springjavalee.model.Usuario;
+import br.javalee.springjavalee.models.Usuario;
 
 public interface UsuarioRepo extends CrudRepository<Usuario, Integer> {
     public Usuario findByEmailOrRacf(String email, String racf);
