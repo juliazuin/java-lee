a92418d70819356a5f759196d852bddc27a786b6
Julia Zuin <zuinjulia@hotmail.com>
2021-11-30 21:56:29 -0300

    ajustes na classe main

--- springjavalee/src/main/java/br/javalee/springjavalee/SpringjavaleeApplication.java
+++ springjavalee/src/main/java/br/javalee/springjavalee/SpringjavaleeApplication.java
@@ -2,14 +2,8 @@ package br.javalee.springjavalee;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
-@ComponentScan({"com.delivery.request"})
-@EntityScan("com.delivery.domain")
-@EnableJpaRepositories("com.delivery.repository")
 public class SpringjavaleeApplication {
 
 	public static void main(String[] args) {
